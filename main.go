7d6c34eeb2544e434484a0924dbac330f65227e4
xevxx <16824503+xevxx@users.noreply.github.com>
2023-08-28 18:49:09 +0100

    Update main.go
    allow multidomain as a comma seperated list

--- main.go
+++ main.go
@@ -4,33 +4,40 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		fmt.Println(e)
-	}
-}
+// func check(e error) {
+// 	if e != nil {
+// 		fmt.Println(e)
+// 	}
+// }
 
 func main() {
 
 	value := flag.String("value", "", "IP to update domain to, example 192.168.1.1")
 	flag.Parse()
 
-	c := config()
-	recordtype := "A"
-	newvalue := *value
+	c, err := config()
+	if err != nil {
+		fmt.Println("Error reading config:", err)
+		os.Exit(1)
+	}
+	recordType := "A"
+	newValue := *value
 
-	if newvalue == "" {
+	if newValue == "" {
 		fmt.Println("You must provide a value for the IP")
 		os.Exit(1)
 	}
 
-	tid, status, message := updateDns(c.ApiKey, c.Password, c.Domain, recordtype, newvalue)
-	if message != "" {
-		fmt.Printf("TransactID: %s, Status: %s, Message: %s\n", tid, status, message)
-	} else {
-		fmt.Printf("TransactID: %s, Status: %s\n", tid, status)
+	domainList := strings.Split(c.Domain, ",")
+	for _, domain := range domainList {
+		tid, status, message := updateDns(c.Url, c.ApiKey, c.Password, domain, recordType, newValue)
+		if message != "" {
+			fmt.Printf("Domain: %s, TransactID: %s, Status: %s, Message: %s\n", domain, tid, status, message)
+		} else {
+			fmt.Printf("Domain: %s, TransactID: %s, Status: %s\n", domain, tid, status)
+		}
 	}
-
 }
