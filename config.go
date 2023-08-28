89d390687e7f4aa8159fa58e7126a952469d9c5e
xevxx <16824503+xevxx@users.noreply.github.com>
2023-08-28 18:38:38 +0100

    Update config.go
    changed to look for config in current folder then default config folders (userconfigdir)- config should be called config.yaml
    
    On Linux, it might return something like /home/username/.config.
    On macOS, it could return /Users/username/Library/Application Support.
    On Windows, it might return C:\Users\username\AppData\Roaming.

--- config.go
+++ config.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/ghodss/yaml"
-	"io/ioutil"
 )
 
-func config() (credents Config) {
-	config_file, err := ioutil.ReadFile("/opt/ibsdns/config.yaml")
-	check(err)
-	yaml.Unmarshal(config_file, &credents)
-	return
+func config() (credents Config, err error) {
+	// Check for a local config.yaml in the current folder
+	localConfigPath := "config.yaml"
+	_, localErr := os.Stat(localConfigPath)
+	if localErr == nil {
+		localConfigFile, localErr := os.ReadFile(localConfigPath)
+		if localErr != nil {
+			return credents, localErr
+		}
+
+		err = yaml.Unmarshal(localConfigFile, &credents)
+		if err != nil {
+			return credents, err
+		}
+
+		return credents, nil
+	}
+
+	// Fallback to user-specific configuration directory
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return credents, err
+	}
+
+	configPath := filepath.Join(configDir, "ibsdns", "config.yaml")
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		return credents, err
+	}
+
+	err = yaml.Unmarshal(configFile, &credents)
+	if err != nil {
+		return credents, err
+	}
+
+	return credents, nil
 }
 
 type Config struct {
 	ApiKey   string `json:"apiKey"`
 	Password string `json:"password"`
 	Domain   string `json:"domain"`
+	Url      string `json:"url"`
 }
